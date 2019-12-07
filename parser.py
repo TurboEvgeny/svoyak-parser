fe4770b03e149da404341061909bc157b3eb1daf
evgeny <mail@mail.ru>
2019-12-07 18:53:05 +0300

    debugging
    now showing all questions' themes

--- parser.py
+++ parser.py
@@ -16,13 +16,17 @@ questionsData = []
 for element in tags:
     question = element.find('span', {"class" : "mw-headline"})
     # узнаем тему и количество баллов
-    print(question.text)
+    #print(question.text)
     match = re.search(r"\((.*)\)", question.text)
     if (match):
         valueStr = match.group(1)
         theme = re.sub(r"\((.*)\)", "", question.text)
         value = int(valueStr.replace(" ", ""))
-        print(theme)
-        print(value)
-    else:
-        print("no match") 
+        #print("-question-")
+        print("%s- %s" % (theme, value))
+        #nextTag = element.next_sibling
+        #print("-body-")
+        #print(element)
+        #print(nextTag)
+        #print(nextTag.next_sibling)
+        #print("---")
